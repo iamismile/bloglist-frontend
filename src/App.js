437ba2b4647a8b3439792f50f815a16bb8511dd6
Ismile <iamismile.dev@gmail.com>
2020-10-15 07:03:25 +0600

    Implement login functionality

--- src/App.js
+++ src/App.js
@@ -1,17 +1,70 @@
 import React, { useState, useEffect } from 'react';
 import Blog from './components/Blog';
 import blogService from './services/blog';
+import loginService from './services/login';
 
 function App() {
   const [blogs, setBlogs] = useState([]);
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [user, setUser] = useState(null);
 
   useEffect(() => {
-    blogService.getAll().then((blogs) => setBlogs(blogs));
+    const getAllBlogs = async () => {
+      const blogs = await blogService.getAll();
+      setBlogs(blogs);
+    };
+
+    getAllBlogs();
   }, []);
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    try {
+      const user = await loginService.login({ username, password });
+      setUser(user);
+      setUsername('');
+      setPassword('');
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  if (user === null) {
+    return (
+      <div>
+        <h2>Login to Application</h2>
+        <form onSubmit={handleLogin}>
+          <div>
+            username{' '}
+            <input
+              type="text"
+              name="Username"
+              value={username}
+              onChange={({ target }) => setUsername(target.value)}
+            />
+          </div>
+          <div>
+            password{' '}
+            <input
+              type="password"
+              name="Password"
+              value={password}
+              onChange={({ target }) => setPassword(target.value)}
+            />
+          </div>
+          <button type="submit">Login</button>
+        </form>
+      </div>
+    );
+  }
 
   return (
     <div>
       <h2>Blogs</h2>
+
+      <p>{user.name} is logged-in</p>
 
       {blogs.map((blog) => (
         <Blog key={blog.id} blog={blog} />
