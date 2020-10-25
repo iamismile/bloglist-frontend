ecedd615a9475c91c0875095cb6142dc3acdc312
Ismile <iamismile.dev@gmail.com>
2020-10-25 10:22:00 +0600

    Changes in Blog Component

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,9 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Blog({ blog }) {
+  const [visible, setVisible] = useState(false);
+
+  const showWhenVisible = { display: visible ? '' : 'none' };
+  const buttonText = visible ? 'Hide' : 'View';
+
+  const toggleVisibility = () => {
+    setVisible(!visible);
+  };
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 5,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5,
+  };
+
   return (
-    <div>
-      {blog.title} - {blog.author}
+    <div style={blogStyle}>
+      <h4>
+        {blog.title} <button onClick={toggleVisibility}>{buttonText}</button>
+      </h4>
+      <div style={showWhenVisible}>
+        <p>{blog.url}</p>
+        <p>
+          Likes {blog.likes} <button>like</button>
+        </p>
+        <p>{blog.author}</p>
+      </div>
     </div>
   );
 }
