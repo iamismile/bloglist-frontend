0f64291d47d348a47a36c8c9dc2686b9d1b9a567
Ismile <iamismile.dev@gmail.com>
2021-02-15 08:26:32 +0600

    E2E Test: Login

--- cypress/integration/blog_app.spec.js
+++ cypress/integration/blog_app.spec.js
@@ -1,6 +1,12 @@
 describe('Blog app', function () {
   beforeEach(function () {
     cy.request('POST', 'http://localhost:3003/api/testing/reset');
+    const user = {
+      username: 'iamismile',
+      name: 'Ismile Hossain',
+      password: 'sekret',
+    };
+    cy.request('POST', 'http://localhost:3003/api/users', user);
     cy.visit('http://localhost:3000');
   });
 
@@ -9,5 +15,21 @@ describe('Blog app', function () {
     cy.get('#username').parent().should('contain', 'username');
     cy.get('#password').parent().should('contain', 'password');
     cy.get('#login-button').contains('Login');
+  });
+
+  describe('Login', function () {
+    it('succeds with correct credentials', function () {
+      cy.get('#username').type('iamismile');
+      cy.get('#password').type('sekret');
+      cy.get('#login-button').click();
+      cy.contains('Ismile Hossain is logged-in');
+    });
+
+    it('fails with worng credentials', function () {
+      cy.get('#username').type('iamismile');
+      cy.get('#password').type('wrong-credentials');
+      cy.get('#login-button').click();
+      cy.get('.error').should('contain', 'Wrong Credentials');
+    });
   });
 });
